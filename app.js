a375a7bbff9ffee23421b269850a6f161196b5e2
Nelson <n911luna@gmail.com>
2022-10-24 14:21:40 -0500

    added static routing

--- app.js
+++ app.js
@@ -3,25 +3,44 @@ const morgan = require("morgan");
 const postBank = require("./postBank");
 const app = express();
 
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(morgan('dev'))
 app.get("/", (req, res) => {
   
-  const post = postBank.list();
+  const posts = postBank.list();
 
   const html = `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-  <ul>
-    ${post.map(post => `<li>${post.title }</li>`)}
-  </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
   </body>
-  </html>`
+</html>`
 
   res.send(html)
 });
+
+
+
+
 
 const PORT = 1337;
 
