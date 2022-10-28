7d59fd8403d99c4c2299fa7e530927b86b01d659
Nelson <n911luna@gmail.com>
2022-10-28 14:08:32 -0500

    fixed node-time-ago and postDetails

--- app.js
+++ app.js
@@ -2,42 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const postBank = require("./postBank");
 const app = express();
-
 const path = require('path');
-const { nextTick } = require("process");
+const postList = require("./views/postList")
+const postDetails = require("./views/postDetails")
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(morgan('dev'))
-app.get("/", (req, res) => {
-  
-  const posts = postBank.list();
 
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map(post => `
-        <div class='news-item'>
-          <p>     
-            <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>`
-      ).join('')}
-    </div>
-  </body>
-</html>`
-
-  res.send(html)
-});
+app.use("/",require("./views/postList"))
 
 app.get('/posts/:id', (req, res,next) => {
   const id = req.params.id
@@ -50,40 +24,18 @@ app.get('/posts/:id', (req, res,next) => {
     next()
   }
 }) 
-  
-  
-  app.get('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const post = postBank.find(id);
-  const html = `<!DOCTYPE html>
-  <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-   </head>
-   <body>
-   <div class="news-list">  
-   <header><img src="/logo.png"/>Wizard News</header>
-     <div class='news-item'>
-     <p>     
-       <span class="news-position">${post.id}. ▲</span>${post.title}
-       <small>(by ${post.name})</small>
-     </p>
-     <p>${post.content}</p>
-     <small class="news-info">
-       ${post.upvotes} upvotes | ${post.date}
-     </small>
-   </div>
-    </div>
-   </body>
-  </html>`
-  res.send(html);
-});
+
+ app.get('/posts/:id',(req,res) => {
+  const id = req.params.id
+  const post = postBank.find(id)
+  res.send(postDetails(post))
+
+ })
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 
- 
 });
---
+++ views/postDetails.js
@@ -0,0 +1,37 @@
+// const express = require('express')
+// const postBank = require("../postBank")
+// const postDetailsRouter = express.Router()
+
+function postDetails(post){
+
+const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+ </head>
+ <body>
+ <div class="news-list">  
+ <header><img src="/logo.png"/>Wizard News</header>
+   <div class='news-item'>
+   <p>     
+     <span class="news-position">${post.id}. ▲</span>${post.title}
+     <small>(by ${post.name})</small>
+   </p>
+   <p>${post.content}</p>
+   <small class="news-info">
+     ${post.upvotes} upvotes | ${post.date}
+   </small>
+ </div>
+  </div>
+ </body>
+</html>`
+
+
+return html
+}
+
+
+
+
+module.exports = postDetails
